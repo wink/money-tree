6a320d38912f8a38e019a164a98aed88099facd8
Micah Winkelspecht <winkelspecht@gmail.com>
2013-09-09 13:44:30 -0700

    Adding coveralls code cov

--- spec/lib/money-tree/node_spec.rb
+++ spec/lib/money-tree/node_spec.rb
@@ -1,3 +1,4 @@
+require 'spec_helper'
 require 'money-tree'
 
 # Test vectors from https://en.bitcoin.it/wiki/BIP_0032_TestVectors
--- spec/lib/money-tree/support_spec.rb
+++ spec/lib/money-tree/support_spec.rb
@@ -1,3 +1,4 @@
+require 'spec_helper'
 require 'money-tree'
 include MoneyTree::Support
 
