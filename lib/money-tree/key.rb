e17fad70eab8ba8858f4290b60e28485af50773d
Matt Smith <matt.the.smith@gmail.com>
2015-05-14 16:17:29 -0700

    trailing whitespace

--- lib/money-tree/key.rb
+++ lib/money-tree/key.rb
@@ -13,10 +13,10 @@ module MoneyTree
     class KeyFormatNotFound < Exception; end
     class InvalidWIFFormat < Exception; end
     class InvalidBase64Format < Exception; end
-    
+
     attr_reader :options, :key, :raw_key
     attr_accessor :ec_key
-    
+
     GROUP_NAME = 'secp256k1'
     ORDER = "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141".to_i(16)
 
@@ -24,18 +24,18 @@ module MoneyTree
       eckey ||= ec_key
       eckey.nil? ? false : eckey.check_key
     end
-    
+
     def to_bytes
       hex_to_bytes to_hex
     end
-    
+
     def to_i
       bytes_to_int to_bytes
     end
   end
-  
+
   class PrivateKey < Key
-        
+
     def initialize(opts = {})
       @options = opts
       @ec_key = PKey::EC.new GROUP_NAME
@@ -48,34 +48,34 @@ module MoneyTree
         @key = to_hex
       end
     end
-    
+
     def generate
       ec_key.generate_key
     end
-    
+
     def import
       ec_key.private_key = BN.new(key, 16)
       set_public_key
     end
-    
+
     def calculate_public_key(opts = {})
       opts[:compressed] = true unless opts[:compressed] == false
       group = ec_key.group
       group.point_conversion_form = opts[:compressed] ? :compressed : :uncompressed
       point = group.generator.mul ec_key.private_key
     end
-    
+
     def set_public_key(opts = {})
       ec_key.public_key = calculate_public_key(opts)
     end
-    
+
     def parse_raw_key
       result = if raw_key.is_a?(Bignum) then from_bignum
       elsif hex_format? then from_hex
       elsif base64_format? then from_base64
       elsif compressed_wif_format? then from_wif
       elsif uncompressed_wif_format? then from_wif
-      else 
+      else
         raise KeyFormatNotFound
       end
       result.downcase
@@ -89,7 +89,7 @@ module MoneyTree
     def from_hex(hex = raw_key)
       hex
     end
-    
+
     def from_wif(wif = raw_key)
       compressed = wif.length == 52
       validate_wif(wif)
@@ -106,7 +106,7 @@ module MoneyTree
     def compressed_wif_format?
       wif_format?(:compressed)
     end
-    
+
     def uncompressed_wif_format?
       wif_format?(:uncompressed)
     end
@@ -120,15 +120,15 @@ module MoneyTree
     def base64_format?(base64_key = raw_key)
       base64_key.length == 44 && base64_key =~ /^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/
     end
-    
+
     def hex_format?
       raw_key.length == 64 && !raw_key[/\H/]
     end
-    
+
     def to_hex
       int_to_hex @ec_key.private_key, 64
     end
-    
+
     def to_wif(compressed: true, network: :bitcoin)
       source = NETWORKS[network][:privkey_version] + to_hex
       source += NETWORKS[network][:privkey_compression_flag] if compressed
@@ -148,24 +148,24 @@ module MoneyTree
       hash_checksum = hash.slice(0..7)
       checksum == hash_checksum
     end
-    
+
     def validate_wif(wif)
       raise InvalidWIFFormat unless wif_valid?(wif)
     end
-    
+
     def to_base64
       encode_base64(to_hex)
     end
-    
+
     def to_s(network: :bitcoin)
       to_wif(network: network)
     end
-    
+
   end
-  
+
   class PublicKey < Key
     attr_reader :private_key, :point, :group, :key_int
-    
+
     def initialize(p_key, opts = {})
       @options = opts
       @options[:compressed] = true if @options[:compressed].nil?
@@ -182,27 +182,27 @@ module MoneyTree
 
       raise ArgumentError, "Must initialize with a MoneyTree::PrivateKey or a public key value" if @key.nil?
     end
-    
+
     def compression
       @group.point_conversion_form
     end
-    
+
     def compression=(compression_type = :compressed)
       @group.point_conversion_form = compression_type
     end
-    
+
     def compressed
       compressed_key = self.class.new raw_key, options # deep clone
       compressed_key.set_point to_i, compressed: true
       compressed_key
     end
-    
+
     def uncompressed
       uncompressed_key = self.class.new raw_key, options # deep clone
       uncompressed_key.set_point to_i, compressed: false
       uncompressed_key
     end
-    
+
     def set_point(int = to_i, opts = {})
       opts = options.merge(opts)
       opts[:compressed] = true if opts[:compressed].nil?
@@ -211,7 +211,7 @@ module MoneyTree
       @point = PKey::EC::Point.new group, bn
       raise KeyInvalid, 'point is not on the curve' unless @point.on_curve?
     end
-    
+
     def parse_raw_key
       result = if raw_key.is_a?(Bignum)
         set_point raw_key
@@ -219,45 +219,45 @@ module MoneyTree
         set_point hex_to_int(raw_key), compressed: false
       elsif compressed_hex_format?
         set_point hex_to_int(raw_key), compressed: true
-      else 
+      else
         raise KeyFormatNotFound
       end
       to_hex
     end
-    
+
     def hex_format?
       raw_key.length == 130 && !raw_key[/\H/]
     end
-    
+
     def compressed_hex_format?
       raw_key.length == 66 && !raw_key[/\H/]
     end
-    
+
     def to_hex
       int_to_hex to_i, 66
     end
-    
+
     def to_i
       point.to_bn.to_i
     end
-    
+
     def to_ripemd160
       hash = sha256 to_hex
       ripemd160 hash
     end
-    
+
     def to_address(network: :bitcoin)
       hash = to_ripemd160
       address = NETWORKS[network][:address_version] + hash
       to_serialized_base58 address
     end
     alias :to_s :to_address
-    
+
     def to_fingerprint
       hash = to_ripemd160
       hash.slice(0..7)
     end
-    
+
     def to_bytes
       int_to_bytes to_i
     end
--- lib/money-tree/node.rb
+++ lib/money-tree/node.rb
@@ -2,19 +2,19 @@ module MoneyTree
   class Node
     include Support
     extend Support
-    attr_reader :private_key, :public_key, :chain_code, 
-      :is_private, :depth, :index, :parent 
-    
+    attr_reader :private_key, :public_key, :chain_code,
+      :is_private, :depth, :index, :parent
+
     class PublicDerivationFailure < Exception; end
     class InvalidKeyForIndex < Exception; end
     class ImportError < Exception; end
     class PrivatePublicMismatch < Exception; end
-    
+
     def initialize(opts = {})
       opts.each { |k, v| instance_variable_set "@#{k}", v }
     end
 
-    def self.from_bip32(address, has_version: true) 
+    def self.from_bip32(address, has_version: true)
       hex = from_serialized_base58 address
       hex.slice!(0..7) if has_version
       self.new({
@@ -33,9 +33,9 @@ module MoneyTree
     def self.parse_out_key(hex)
       if hex.slice(0..1) == '00'
         private_key = MoneyTree::PrivateKey.new(key: hex.slice(2..-1))
-        { 
+        {
           private_key: private_key,
-          public_key: MoneyTree::PublicKey.new(private_key) 
+          public_key: MoneyTree::PublicKey.new(private_key)
         }
       elsif %w(02 03).include? hex.slice(0..1)
         { public_key: MoneyTree::PublicKey.new(hex) }
@@ -47,7 +47,7 @@ module MoneyTree
     def is_private?
       index >= 0x80000000 || index < 0
     end
-    
+
     def index_hex(i = index)
       if i < 0
         [i].pack('l>').unpack('H*').first
@@ -55,15 +55,15 @@ module MoneyTree
         i.to_s(16).rjust(8, "0")
       end
     end
-    
+
     def depth_hex(depth)
       depth.to_s(16).rjust(2, "0")
     end
-    
+
     def private_derivation_message(i)
       "\x00" + private_key.to_bytes + i_as_bytes(i)
     end
-    
+
     def public_derivation_message(i)
       public_key.to_bytes << i_as_bytes(i)
     end
@@ -71,7 +71,7 @@ module MoneyTree
     def i_as_bytes(i)
       [i].pack('N')
     end
-    
+
     def derive_private_key(i = 0)
       message = i >= 0x80000000 || i < 0 ? private_derivation_message(i) : public_derivation_message(i)
       hash = hmac_sha512 hex_to_bytes(chain_code_hex), message
@@ -82,7 +82,7 @@ module MoneyTree
       child_chain_code = right_from_hash(hash)
       return child_private_key, child_chain_code
     end
-    
+
     def derive_public_key(i = 0)
       raise PrivatePublicMismatch if i >= 0x80000000
       message = public_derivation_message(i)
@@ -95,11 +95,11 @@ module MoneyTree
       child_chain_code = right_from_hash(hash)
       return child_public_key, child_chain_code
     end
-    
+
     def left_from_hash(hash)
       bytes_to_int hash.bytes.to_a[0..31]
     end
-    
+
     def right_from_hash(hash)
       bytes_to_int hash.bytes.to_a[32..-1]
     end
@@ -114,7 +114,7 @@ module MoneyTree
       hex += chain_code_hex
       hex += type.to_sym == :private ? "00#{private_key.to_hex}" : public_key.compressed.to_hex
     end
-    
+
     def to_bip32(type = :public, network: :bitcoin)
       raise PrivatePublicMismatch if type.to_sym == :private && private_key.nil?
       to_serialized_base58 to_serialized_hex(type, network: network)
@@ -129,11 +129,11 @@ module MoneyTree
       key = compressed ? public_key.compressed : public_key.uncompressed
       key.to_ripemd160
     end
-    
+
     def to_fingerprint
       public_key.compressed.to_fingerprint
     end
-    
+
     def parent_fingerprint
       if @parent_fingerprint
         @parent_fingerprint
@@ -146,7 +146,7 @@ module MoneyTree
       address = NETWORKS[network][:address_version] + to_identifier(compressed)
       to_serialized_base58 address
     end
-    
+
     def subnode(i = 0, opts = {})
       if private_key.nil?
         child_public_key, child_chain_code = derive_public_key(i)
@@ -156,25 +156,25 @@ module MoneyTree
         child_private_key = MoneyTree::PrivateKey.new key: child_private_key
         child_public_key = MoneyTree::PublicKey.new child_private_key
       end
-            
-      MoneyTree::Node.new( depth: depth+1, 
-                          index: i, 
+
+      MoneyTree::Node.new( depth: depth+1,
+                          index: i,
                           private_key: private_key.nil? ? nil : child_private_key,
                           public_key: child_public_key,
                           chain_code: child_chain_code,
                           parent: self)
     end
-    
+
     # path: a path of subkeys denoted by numbers and slashes. Use
     #     p or i<0 for private key derivation. End with .pub to force
     #     the key public.
-    # 
+    #
     # Examples:
     #     1p/-5/2/1 would call subkey(i=1, is_prime=True).subkey(i=-5).
     #         subkey(i=2).subkey(i=1) and then yield the private key
     #     0/0/458.pub would call subkey(i=0).subkey(i=0).subkey(i=458) and
     #         then yield the public key
-    # 
+    #
     # You should choose either the p or the negative number convention for private key derivation.
     def node_for_path(path)
       force_public = path[-4..-1] == '.pub'
@@ -198,11 +198,11 @@ module MoneyTree
         nodes.last
       end
     end
-    
+
     def parse_index(path_part)
       is_prime = %w(p ').include? path_part[-1]
       i = path_part.to_i
-      
+
       i = if i < 0
         i
       elsif is_prime
@@ -211,16 +211,16 @@ module MoneyTree
         i & 0x7fffffff
       end
     end
-    
+
     def strip_private_info!
       @private_key = nil
     end
-    
+
     def chain_code_hex
       int_to_hex chain_code, 64
     end
   end
-  
+
   class Master < Node
     module SeedGeneration
       class Failure < Exception; end
@@ -230,12 +230,12 @@ module MoneyTree
       class ImportError < Failure; end
       class TooManyAttempts < Failure; end
     end
-    
+
     HD_WALLET_BASE_KEY = "Bitcoin seed"
     RANDOM_SEED_SIZE = 32
-    
+
     attr_reader :seed, :seed_hash
-    
+
     def initialize(opts = {})
       @depth = 0
       @index = 0
@@ -263,27 +263,27 @@ module MoneyTree
         set_seeded_keys
       end
     end
-    
+
     def is_private?
       true
     end
-    
+
     def generate_seed
       @seed = OpenSSL::Random.random_bytes(32)
       @seed_hash = generate_seed_hash(@seed)
       raise SeedGeneration::ValidityError unless seed_valid?(@seed_hash)
     end
-    
+
     def generate_seed_hash(seed)
       hmac_sha512 HD_WALLET_BASE_KEY, seed
     end
-    
+
     def seed_valid?(seed_hash)
       return false unless seed_hash.bytesize == 64
       master_key = left_from_hash(seed_hash)
       !master_key.zero? && master_key < MoneyTree::Key::ORDER
     end
-    
+
     def set_seeded_keys
       @private_key = MoneyTree::PrivateKey.new key: left_from_hash(seed_hash)
       @chain_code = right_from_hash(seed_hash)
